5eef3dd2069a5df96eb1c575c3b9e22a66f1bc7f
BADIUDragos <dragos.badiu.a@hotmail.com>
2023-11-10 16:31:37 -0500

    implemented idle 15 minutes

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,9 +1,44 @@
-import { Outlet } from "react-router-dom";
+import Idle from "idle-js"
+import { Outlet, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Container } from "react-bootstrap";
+import { useBlacklistMutation } from "../store";
+import { useEffect } from "react";
 
 const Layout: React.FC = () => {
+
+  const navigate = useNavigate()
+  const [blacklistToken] = useBlacklistMutation();
+
+  useEffect(() => {
+    const getRefreshToken = () => {
+      return localStorage.getItem('refreshToken');
+    };
+
+    const idle = new Idle({
+      idle: 900000,
+      onIdle: async () => {
+        const refreshToken = getRefreshToken();
+        if (refreshToken) {
+          try {
+            await blacklistToken({ refresh: refreshToken }).unwrap();
+            
+          } catch (error) {
+            console.error('Error blacklisting token:', error);
+          }
+        }
+      },
+      events: ['mousemove', 'keydown', 'mousedown'],
+    });
+
+    idle.start();
+
+    return () => {
+      idle.stop();
+    };
+  }, [blacklistToken, navigate]);
+
   return (
     <Container fluid className="p-0 d-flex flex-column min-vh-100">
       <Header className="mb-5" />
