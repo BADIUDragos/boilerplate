330ae1d4b584410ca9d137d89ac42a076d709459
BADIUDragos <dragos.badiu.a@hotmail.com>
2023-11-28 03:39:27 -0500

    rewrap in provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,9 @@ import { RouterProvider } from "react-router-dom";
 import "./bootstrap.css";
 
 import router from "./router/router";
-import Idle from "idle-js"
 
-import { useLogoutMutation, useTokens, useValidateQuery } from "./store";
-import { useEffect } from "react";
 
 export default function App() {
-  const [blacklistToken] = useLogoutMutation();
-  const tokens = useTokens();
-
-  useEffect(() => {
-    if (tokens) {
-      const idle = new Idle({
-        idle: 900000,
-        onIdle: async () => {
-          if (tokens) {
-            blacklistToken({ refresh: tokens.refresh });
-          }
-        },
-        events: ["mousemove", "keydown", "mousedown"],
-      });
-
-      idle.start();
-
-      return () => {
-        idle.stop();
-      };
-    }
-  }, [blacklistToken, tokens]);
 
   return <RouterProvider router={router} />;
 }
